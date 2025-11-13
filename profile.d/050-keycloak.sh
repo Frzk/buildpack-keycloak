f7da7fd9ed9a9f707b88a5281d1c9a072632fbca
François KUBLER <francois+github@kubler.org>
2025-11-13 18:03:53 +0100

    feat(profile.d): remove readonly variables

--- profile.d/050-keycloak.sh
+++ profile.d/050-keycloak.sh
@@ -7,13 +7,6 @@ password="$( echo "${SCALINGO_POSTGRESQL_URL}" | cut -d "@" -f1 | cut -d ":" -f3
 database="$( echo "${SCALINGO_POSTGRESQL_URL}" | cut -d "?" -f1 | cut -d "/" -f4 )"
 db_url="jdbc:postgresql://${host}:${port}/${database}"
 
-readonly host
-readonly port
-readonly username
-readonly password
-readonly database
-readonly db_url
-
 export KC_DB="${KC_DB:-"postgres"}"
 export KC_DB_USERNAME="${username}"
 export KC_DB_PASSWORD="${password}"
